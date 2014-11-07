082c87c1e302449efaffd391426c2ec45703fdaa
rmylonas <roman.mylonas@isb-sib.ch>
2014-11-07 15:43:28 +0100

    parsing

--- app/ch/isbsib/proteomics/mzviz/importer/ImporterMzIdent.scala
+++ app/ch/isbsib/proteomics/mzviz/importer/ImporterMzIdent.scala
@@ -1,17 +1,38 @@
 package ch.isbsib.proteomics.mzviz.importer
 
+import java.io.{FileInputStream, InputStream}
+import java.util
+
+import org.expasy.mzjava.proteomics.io.ms.ident.{PSMReaderCallback, MzIdentMlReader}
+import org.expasy.mzjava.proteomics.ms.ident.{SpectrumIdentifier, PeptideMatch}
+
+import scala.collection.mutable.ListBuffer
+
 /**
  * @author Alexandre Masselot
  */
 object ImporterMzIdent {
+
   /**
    * parse a file and return a full run.
    * Well, for the moment, it does not much :)
    * @param filename an .mzid file
    * @return
    */
-  def parse(filename: String)= {
-    42
-    new Pipo().hihi()
+  def parse(filename: String) = {
+    val searchResults = ListBuffer[Tuple2[SpectrumIdentifier, PeptideMatch]]();
+
+    val insertIdResultCB: PSMReaderCallback = new PSMReaderCallback {
+      def resultRead(identifier: SpectrumIdentifier, peptideMatch: PeptideMatch) = searchResults.append(Tuple2(identifier, peptideMatch))
+    }
+
+    val fr: InputStream = new FileInputStream(filename)
+    val reader: MzIdentMlReader = new MzIdentMlReader
+
+    reader.parse(fr, insertIdResultCB)
+    val searchResultMap  = searchResults.groupBy(_._1)
+    for {(spId, list) <- searchResultMap} {
+      println(spId, list)
+    }
   }
 }
---
+++ app/ch/isbsib/proteomics/mzviz/models/SpectrumId.scala
@@ -0,0 +1,8 @@
+package ch.isbsib.proteomics.mzviz.models
+
+/**
+ * Created by admin on 07/11/14.
+ */
+case  class SpectrumId(scanNumber:Int, retentionTime:Double) {
+
+}
