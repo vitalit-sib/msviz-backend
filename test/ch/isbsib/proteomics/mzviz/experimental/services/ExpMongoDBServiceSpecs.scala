1131af68e9cc3cb75faf921b1ecbbbb4e6385db1
tmartinc <trinidad.martin@isb-sib.ch>
2014-11-28 15:35:22 +0100

    Fasta file with 2 sequences

--- test/ch/isbsib/proteomics/mzviz/experimental/services/ExpMongoDBServiceSpecs.scala
+++ test/ch/isbsib/proteomics/mzviz/experimental/services/ExpMongoDBServiceSpecs.scala
@@ -1,5 +1,6 @@
 package ch.isbsib.proteomics.mzviz.experimental.services
 
+import ch.isbsib.proteomics.mzviz.commons.TempMongoDBServiceForSpecs
 import ch.isbsib.proteomics.mzviz.experimental.IdRun
 import ch.isbsib.proteomics.mzviz.experimental.importer.LoaderMGF
 import org.scalatest.concurrent.ScalaFutures
@@ -7,10 +8,7 @@ import org.scalatest.time.{Millis, Seconds, Span}
 import org.specs2.execute.AsResult
 import org.specs2.mutable.{Around, Specification}
 import reactivemongo.api.MongoDriver
-import reactivemongo.core.commands.GetLastError
 
-import scala.concurrent.Await
-import scala.concurrent.duration._
 import scala.util.Random
 
 /**
@@ -23,45 +21,16 @@ class ExpMongoDBServiceSpecs extends Specification with ScalaFutures {
   implicit val defaultPatience =
     PatienceConfig(timeout = Span(2, Seconds), interval = Span(5, Millis))
 
-  def createDB(dbNamePefix: String, host: String = "localhost:27017") = {
-    val driver = new MongoDriver
-    val connection = driver.connection(List(host))
 
-    val dbName = s"$dbNamePefix-${new Random().nextLong}"
-    println(s"creating a mongodb named $dbName")
-    new ExpMongoDBService(connection.db(dbName))
-  }
-
-  def dropDBService(service: ExpMongoDBService) = {
-    println(s"dropped ${service.db.name}")
-    service.db.drop()
-  }
-
-  trait tmpService extends Around {
-    val service = createDB("test")
-
-    override def around[T: AsResult](t: => T) = {
-      val r = try {
-        AsResult.effectively(t)
-      } catch {
-        case e: Throwable => {
-          //preform some logic here
-          throw e
-        }
-      }
-      dropDBService(service)
-      r
-    }
-  }
 
   "empty service" should {
-    "counts are 0" in new tmpService {
+    "counts are 0" in new TempMongoDBServiceForSpecs {
       service.countMsnSpectra.futureValue must equalTo(0)
       service.countMsRuns.futureValue must equalTo(0)
     }
   }
   "create 2 runs" should {
-    "get them up " in new tmpService {
+    "get them up " in new TempMongoDBServiceForSpecs {
       //      "solve the destory db firest" >> pending {
       service.countMsnSpectra.futureValue must equalTo(0)
       service.countMsRuns.futureValue must equalTo(0)
