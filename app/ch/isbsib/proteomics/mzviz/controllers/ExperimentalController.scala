16bddde778d9ece707cb0d9870eb52a9ed01118a
Alexandre Masselot <alexandre.masselot@isb-sib.ch>
2014-12-05 08:45:45 +0100

    added swagger- although test call do not work

--- app/ch/isbsib/proteomics/mzviz/controllers/ExperimentalController.scala
+++ app/ch/isbsib/proteomics/mzviz/controllers/ExperimentalController.scala
@@ -1,12 +1,14 @@
 package ch.isbsib.proteomics.mzviz.controllers
 
 import java.io.File
+import javax.ws.rs.PathParam
 
 import ch.isbsib.proteomics.mzviz.experimental.IdRun
 import ch.isbsib.proteomics.mzviz.experimental.importer.LoaderMGF
 import ch.isbsib.proteomics.mzviz.experimental.models.{RefSpectrum, ExpMSnSpectrum}
 import ch.isbsib.proteomics.mzviz.experimental.services.ExpMongoDBService
 import ch.isbsib.proteomics.mzviz.experimental.services.JsonFormats._
+import com.wordnik.swagger.annotations._
 import play.api.libs.Files
 import play.api.mvc.{MultipartFormData, Request, Action, Controller}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
@@ -27,6 +29,7 @@ import play.modules.reactivemongo.json.collection.JSONCollection
 /**
  * @author Alexandre Masselot
  */
+@Api(value = "/exp", description = "hg sequence access")
 object ExperimentalController extends Controller {
 
   def stats = Action.async {
@@ -48,12 +51,25 @@ object ExperimentalController extends Controller {
     }
   }
 
+  @ApiOperation(nickname = "listMSRunIds",
+    value = "the list of run ids",
+    notes = """from the parameter run-id at load time""",
+    response = classOf[List[String]],
+    httpMethod = "GET")
   def listMSRunIds = Action.async {
     ExpMongoDBService().listMsRunIds.map {
       ids => Ok(Json.obj("msRuns" -> ids.map(_.value)))
     }
   }
 
+  @ApiOperation(nickname = "loadMSRun",
+    value = "Loads an MGF peak list as a run",
+    notes = """ run-id will be important to link with the mzid""",
+    response = classOf[String],
+    httpMethod = "POST")
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name = "mgf", value = "mgf peak list", required = true, dataType = "file", paramType = "body")
+  ))
   def loadMSRun = Action.async(parse.multipartFormData) {
     request =>
       localFile("mgf", request)
@@ -69,25 +85,42 @@ object ExperimentalController extends Controller {
       }
   }
 
-
-  def findExpSpectrum(idRun: String, title: String) = Action.async {
-    ExpMongoDBService().findSpectrumByRunIdAndTitle(IdRun(idRun), title)
-      .map { case sp: ExpMSnSpectrum => Ok(Json.toJson(sp))}
-      .recover {
-      case e => BadRequest(e.getMessage+e.getStackTrace.mkString("\n"))
+  @ApiOperation(nickname = "findExpSpectrum",
+    value = "find a spectrum by run id and spectrum title",
+    notes = """the tuple should be unique by indexing""",
+    response = classOf[ExpMSnSpectrum],
+    httpMethod = "GET")
+  def findExpSpectrum(@ApiParam(value = """run id""", defaultValue = "") @PathParam("idRun") idRun: String,
+                      @ApiParam(value = """spectrum title""", defaultValue = "") @PathParam("title") title: String) =
+    Action.async {
+      ExpMongoDBService().findSpectrumByRunIdAndTitle(IdRun(idRun), title)
+        .map { case sp: ExpMSnSpectrum => Ok(Json.toJson(sp))}
+        .recover {
+        case e => BadRequest(e.getMessage + e.getStackTrace.mkString("\n"))
+      }
     }
-  }
 
-  def findAllRefSpectraByIdRun(idRun: String) = Action.async {
-    ExpMongoDBService().findAllRefSpectraByIdRun(IdRun(idRun))
-      .map { case sphList: List[JsObject] => Ok(Json.toJson(sphList))}
-      .recover {
-      case e => BadRequest(e.getMessage+e.getStackTrace.mkString("\n"))
+  @ApiOperation(nickname = "findAllRefSpectraByIdRun",
+    value = "find all spectra for a given run id",
+    notes = """Returns only the reference information (precursor & co)""",
+    response = classOf[List[RefSpectrum]],
+    httpMethod = "GET")
+  def findAllRefSpectraByIdRun(@ApiParam(value = """run id""", defaultValue = "") @PathParam("idRun") idRun: String) =
+    Action.async {
+      ExpMongoDBService().findAllRefSpectraByIdRun(IdRun(idRun))
+        .map { case sphList: List[JsObject] => Ok(Json.toJson(sphList))}
+        .recover {
+        case e => BadRequest(e.getMessage + e.getStackTrace.mkString("\n"))
+      }
     }
-  }
 
-  def deleteMSRun(id: String) = Action.async {
-    ExpMongoDBService().delete(IdRun(id)).map { x =>
+  @ApiOperation(nickname = "deleteMSRun",
+    value = "delete a run and all experimental data associated with it",
+    notes = """No double check is done. Use with caution""",
+    response = classOf[String],
+    httpMethod = "DELETE")
+  def deleteMSRun(idRun: String) = Action.async {
+    ExpMongoDBService().delete(IdRun(idRun)).map { x =>
       Ok("OK")
     }
   }
