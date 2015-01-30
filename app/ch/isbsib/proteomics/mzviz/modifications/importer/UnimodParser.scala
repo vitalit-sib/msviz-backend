3f95e3d803ea4c4b6fb7abd68498a53a7f37198e
tmartinc <trinidad.martin@isb-sib.ch>
2015-01-30 16:49:40 +0100

    getSize method

--- app/ch/isbsib/proteomics/mzviz/modifications/importer/UnimodParser.scala
+++ app/ch/isbsib/proteomics/mzviz/modifications/importer/UnimodParser.scala
@@ -1,6 +1,6 @@
 package ch.isbsib.proteomics.mzviz.modifications.importer
 
-import ch.isbsib.proteomics.mzviz.theoretical.{NumDatabaseSequences, SequenceSource}
+
 import scala.xml.XML
 
 /**
@@ -9,9 +9,9 @@ import scala.xml.XML
  * @author Roman Mylonas, Trinidad Martin & Alexandre Masselot
  * copyright 2014-2015, SIB Swiss Institute of Bioinformatics
  */
-class UnimodParser {
-  //val filename="test/resources/unimod.xml"
-  val unimod = XML.load("http://mascot.vital-it.ch/mascot/cgi/get_params.pl?Show=MS_UNIMODXML")
+class UnimodParser (file:String) {
+  //val filename="http://mascot.vital-it.ch/mascot/cgi/get_params.pl?Show=MS_UNIMODXML"
+  val unimod = XML.load(file)
 
   val key=(unimod \\ "mod").map { mod => (mod \ "@full_name").text}
   val tuple=(unimod \\ "mod").map { mod =>
@@ -19,5 +19,30 @@ class UnimodParser {
   }
 
   val dictionary=(key zip tuple).toMap
+
+
+  /**
+   * return number of entries
+   * @return
+   */
+  def getSize:Int={
+    dictionary.size
+  }
+
+  /**
+   * return value for a given key
+   * @return
+   */
+  //def getValues(key:String){
+  //  dictionary.get(key)
+  //}
+
+}
+
+/**
+ * companion object
+ */
+object UnimodParser {
+  def apply(filename: String) = new UnimodParser(filename)
 
 }
