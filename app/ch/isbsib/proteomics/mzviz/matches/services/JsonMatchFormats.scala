cde8eea7905094d4362fe88c012d4d5905ce81cf
Alexandre Masselot <alexandre.masselot@isb-sib.ch>
2015-04-08 06:48:16 +0200

    ProteinRef with optional 'identifiers' field - shuld find a better solution

--- app/ch/isbsib/proteomics/mzviz/matches/services/JsonMatchFormats.scala
+++ app/ch/isbsib/proteomics/mzviz/matches/services/JsonMatchFormats.scala
@@ -12,17 +12,16 @@ import ch.isbsib.proteomics.mzviz.modifications.services.JsonModificationFormats
 
 /**
  * @author Roman Mylonas, Trinidad Martin & Alexandre Masselot
- * copyright 2014-2015, SIB Swiss Institute of Bioinformatics
+ *         copyright 2014-2015, SIB Swiss Institute of Bioinformatics
  */
 
 object JsonMatchFormats {
 
 
-
   implicit val formatSpectraId = new Format[SpectrumId] {
     override def reads(json: JsValue): JsResult[SpectrumId] = JsSuccess(SpectrumId(SpectrumUniqueId((json \ "id").as[String]), RunId((json \ "runId").as[String])))
 
-    def writes(o: SpectrumId) = Json.obj("id"->o.id.value, "runId"->o.runId.value)
+    def writes(o: SpectrumId) = Json.obj("id" -> o.id.value, "runId" -> o.runId.value)
   }
 
   implicit val formatAccessionCode = new Format[AccessionCode] {
@@ -48,7 +47,25 @@ object JsonMatchFormats {
     def writes(o: SearchId) = JsString(o.value)
   }
 
-  implicit val formatProteinRef = Json.format[ProteinRef]
+  implicit val formatProteinRef = new Format[ProteinRef] {
+    //OK, that starts to be ugly. identifiers field might not have been defined for everyone...
+    override def reads(json: JsValue): JsResult[ProteinRef] = {
+      JsSuccess(ProteinRef(
+        AC = AccessionCode((json \ "AC").as[String]),
+        identifiers = (json \ "identifiers").asOpt[Set[String]].getOrElse(Set()).map(ProteinIdentifier.apply),
+        source = (json \ "source").asOpt[String].map(SequenceSource.apply)
+      ))
+    }
+
+    def writes(o: ProteinRef) = Json.obj(
+      "AC" -> o.AC,
+      "identifiers" -> o.identifiers,
+      "source" -> o.source
+    )
+
+  }
+
+
   implicit val formatProteinMatch = Json.format[ProteinMatch]
   implicit val formatPeptide = Json.format[Peptide]
   implicit val formatPepMatchInfo = Json.format[PepMatchInfo]
