9054753d64896b66352e054c3d31dfee3824c5a2
Trinidad Martín <trinidad.martin@isb-sib.ch>
2015-04-10 15:19:14 +0200

    SearchDatabase class with id, version and entries

---
+++ app/ch/isbsib/proteomics/mzviz/theoretical/models/SearchDatabase.scala
@@ -0,0 +1,10 @@
+import ch.isbsib.proteomics.mzviz.matches.SearchId
+
+/**
+ * @author Roman Mylonas, Trinidad Martin & Alexandre Masselot
+ * copyright 2014-2015, SIB Swiss Institute of Bioinformatics
+ */
+case class SearchDatabase (id: String,
+                           version:String,
+                           entries:String)
+
