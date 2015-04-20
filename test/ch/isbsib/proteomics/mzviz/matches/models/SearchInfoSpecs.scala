6d9b907dd48576f7bc7ec24adbce053b990b59ea
Trinidad Martín <trinidad.martin@isb-sib.ch>
2015-04-20 14:52:53 +0200

    Test for SearInfo parser

---
+++ test/ch/isbsib/proteomics/mzviz/matches/models/SearchInfoSpecs.scala
@@ -0,0 +1,45 @@
+package ch.isbsib.proteomics.mzviz.matches.models
+
+import java.io.File
+
+import ch.isbsib.proteomics.mzviz.commons.TempMongoDBForSpecs
+import ch.isbsib.proteomics.mzviz.matches.SearchId
+import ch.isbsib.proteomics.mzviz.matches.importer.LoaderMzIdent
+import ch.isbsib.proteomics.mzviz.matches.services.SearchInfoDBService
+import ch.isbsib.proteomics.mzviz.theoretical.models.SearchDatabase
+import ch.isbsib.proteomics.mzviz.theoretical.services.SequenceMongoDBService
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Millis, Seconds, Span}
+import org.specs2.mutable.Specification
+
+/**
+ * @author Roman Mylonas, Trinidad Martin & Alexandre Masselot
+ * copyright 2014-2015, SIB Swiss Institute of Bioinformatics
+ */
+class SearchInfoSpecs extends Specification with ScalaFutures {
+  implicit val defaultPatience =
+    PatienceConfig(timeout = Span(15, Seconds), interval = Span(5000, Millis))
+
+  /**
+   * extends the temp mongodatabase and add a exp service above it
+   */
+  trait TempMongoDBService extends TempMongoDBForSpecs {
+    val service = new SearchInfoDBService(db)
+  }
+
+
+  "parser" should {
+    "parse title, database, username" in new TempMongoDBService {
+      val searchEntries =LoaderMzIdent.parseSearchInfo(new File("test/resources/M_100.mzid"), SearchId("M_100"))
+      print (searchEntries)
+      val title = searchEntries.title
+      val databaseName = searchEntries.database
+      val username= searchEntries.username
+
+      title mustEqual("test rafts sample 123 spectra for Roman")
+      databaseName mustEqual List(SearchDatabase("SDB_SwissProt_ID","SwissProt_2014_08.fasta",546238))
+      username mustEqual("roman")
+    }
+  }
+
+}
