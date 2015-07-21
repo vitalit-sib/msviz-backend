6c92799e2fa1a54d9cde5dad72eda5b6048e4c4d
rmylonas <roman.mylonas@isb-sib.ch>
2015-07-21 14:45:59 +0200

    corrected comment

--- app/ch/isbsib/proteomics/mzviz/experimental/importer/MGFParsingException.scala
+++ app/ch/isbsib/proteomics/mzviz/experimental/importer/MGFParsingException.scala
@@ -1,6 +1,7 @@
 package ch.isbsib.proteomics.mzviz.experimental.importer
 
 /**
- * Created by amasselo on 3/6/15.
+ * @author Roman Mylonas, Trinidad Martin & Alexandre Masselot
+ *         copyright 2014-2015, SIB Swiss Institute of Bioinformatics
  */
 case class MGFParsingException( message:String,  cause:Throwable=null) extends Throwable(message, cause)
