f822fb7922cf7593f6f15b19be00af62d017f6c6
qinfang jolliet <shinhou.ko@gmail.com>
2015-08-03 15:18:16 +0200

    modify the qc/models/SummaryRef.scala->QcSummaryEntry.scala

--- app/ch/isbsib/proteomics/mzviz/qc/importer/LoadSummary.scala
+++ app/ch/isbsib/proteomics/mzviz/qc/importer/LoadSummary.scala
@@ -3,7 +3,7 @@ import java.io.File
 
 import ch.isbsib.proteomics.mzviz.commons._
 import ch.isbsib.proteomics.mzviz.qc._
-import ch.isbsib.proteomics.mzviz.qc.models.{QcSummaryEntry}
+import ch.isbsib.proteomics.mzviz.qc.models.{RawfileInfomation, QcSummaryEntry}
 
 import scala.io.Source
 import scala.reflect.io
@@ -23,16 +23,28 @@ class LoadSummary (file:String) {
   val itLines = Source.fromFile(file).getLines()
   //extract the header line to a list*
 
-  def getSummaryEntry:Seq[QcSummaryEntry] = {itLines.toList.drop(1).dropRight(1).map({ s=>
+  def getSummaryEntry:Seq[QcSummaryEntry] = itLines.toList.drop(1).dropRight(1).map({ s=>
       val data = s.split("\t")
       val rawfile = getInfo(data(0))
-      val len= rawfile.length
-    QcSummaryEntry(rawfile(len-2),rawfile(len-1),data(14).toInt,data(15).toInt,data(20).toInt,data(28).toInt)
-  })}
+      val protein = rawfile(0)
+      val quantity = rawfile(1)
+      val machineName = rawfile(2)
+      val columnType = rawfile(3) + "_" + rawfile(4)
+      def isAllDigits(x: String) = x forall Character.isDigit
+      val Date = if (isAllDigits(rawfile(5)))  rawfile(5)
+                 else throw throw new SummaryParsingException(s"It's a value not valide for date:\n$rawfile(5)")
+
+      val Index = rawfile(6)
+
+      val rawfileInfomation=RawfileInfomation(protein,quantity,machineName,columnType,Date,Index)
+
+      QcSummaryEntry(rawfileInfomation,data(14).toInt,data(15).toInt,data(20).toInt,data(28).toInt)
+  })
 
   def getInfo(rawfile:String) = {
     rawfile.split("_")
   }
+
 
 }
 /**
--- app/ch/isbsib/proteomics/mzviz/qc/models/QcSummaryEntry.scala
+++ app/ch/isbsib/proteomics/mzviz/qc/models/QcSummaryEntry.scala
@@ -6,4 +6,6 @@ import ch.isbsib.proteomics.mzviz.qc
 /**
  * Created by qjolliet on 28/07/15.
  */
-case class QcSummaryEntry(Date:String,Index:String,MS:Int,MMS:Int,MmsIdentify:Int,PeptideSeq:Int)
+case class QcSummaryEntry(rawfileInfomation:RawfileInfomation,MS:Int,MMS:Int,MmsIdentify:Int,PeptideSeq:Int)
+
+case class RawfileInfomation(proteinName:String,pQuantity:String,machineName:String,columnType:String,Date:String,Index:String)
