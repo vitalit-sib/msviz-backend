d207b254dbaf682b74bb816ad9c2577d3f04301f
qinfang jolliet <shinhou.ko@gmail.com>
2015-08-04 09:55:50 +0200

    using Map to link file header and values

--- app/ch/isbsib/proteomics/mzviz/qc/importer/LoadSummary.scala
+++ app/ch/isbsib/proteomics/mzviz/qc/importer/LoadSummary.scala
@@ -3,7 +3,7 @@ import java.io.File
 
 import ch.isbsib.proteomics.mzviz.commons._
 import ch.isbsib.proteomics.mzviz.qc._
-import ch.isbsib.proteomics.mzviz.qc.models.{RawfileInfomation, QcSummaryEntry}
+import ch.isbsib.proteomics.mzviz.qc.models.{QcSummaryEntry, RawfileInfomation}
 
 import scala.io.Source
 import scala.reflect.io
@@ -21,8 +21,13 @@ class LoadSummary (file:String) {
    ** @param filename  summary.txt to read from
    **/
   val itLines = Source.fromFile(file).getLines()
-  //extract the header line to a list*
 
+  //extract the header line to a list*
+  val header = itLines.take(1).next.split("\t").toList
+
+  //extract the Summary lines except total line
+  val mSummary = itLines.toList.dropRight(1).map (s => (header zip s.split("\t")).toMap)
+  /***
   def getSummaryEntry:Seq[QcSummaryEntry] = itLines.toList.drop(1).dropRight(1).map({ s=>
       val data = s.split("\t")
       val rawfile = getInfo(data(0))
@@ -30,9 +35,9 @@ class LoadSummary (file:String) {
       val quantity = rawfile(1)
       val machineName = rawfile(2)
       val columnType = rawfile(3) + "_" + rawfile(4)
-      def isAllDigits(x: String) = x forall Character.isDigit
+
       val Date = if (isAllDigits(rawfile(5)))  rawfile(5)
-                 else throw throw new SummaryParsingException(s"It's a value not valide for date:\n$rawfile(5)")
+                 else throw new SummaryParsingException(s"It's a value not valide for date:\n$rawfile(5)")
 
       val Index = rawfile(6)
 
@@ -40,12 +45,28 @@ class LoadSummary (file:String) {
 
       QcSummaryEntry(rawfileInfomation,data(14).toInt,data(15).toInt,data(20).toInt,data(28).toInt)
   })
+  ***/
+  def getSummaryEntry:Seq[QcSummaryEntry] = mSummary.map{
+      m => {
+        var rawfile = getInfo(m("Raw file"))
+        var protein = rawfile(0)
+        var quantity = rawfile(1)
+        var machineName = rawfile(2)
+        var columnType = rawfile(3) + "_" + rawfile(4)
+        var Date = if (isAllDigits(rawfile(5)))  rawfile(5)
+                   else throw new SummaryParsingException(s"It's not a valide date:\n$rawfile(5)")
+        var Index = rawfile(6)
+        var rawfileInfo=RawfileInfomation(protein,quantity,machineName,columnType,Date,Index)
+        QcSummaryEntry(rawfileInfo,m("MS").toInt,m("MS/MS").toInt,m("MS/MS Identified").toInt,m("Peptide Sequences Identified").toInt)
+      }
+  }
+
 
   def getInfo(rawfile:String) = {
     rawfile.split("_")
   }
 
-
+  def isAllDigits(x: String) = x forall Character.isDigit
 }
 /**
  * companion object
