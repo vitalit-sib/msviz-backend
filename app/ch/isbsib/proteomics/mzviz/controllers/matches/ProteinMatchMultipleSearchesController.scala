ddd6d2c962546c1a786e12b560985624997e9fda
Trinidad Martín <trinidad.martin@isb-sib.ch>
2015-09-22 10:45:11 +0200

    new method returning list of ProteinIdents from list of searchIds and listo of Acs

--- app/ch/isbsib/proteomics/mzviz/controllers/matches/ProteinMatchMultipleSearchesController.scala
+++ app/ch/isbsib/proteomics/mzviz/controllers/matches/ProteinMatchMultipleSearchesController.scala
@@ -2,11 +2,13 @@ package ch.isbsib.proteomics.mzviz.controllers.matches
 
 import javax.ws.rs.PathParam
 
+import ch.isbsib.proteomics.mzviz.controllers.matches.PSMController._
 import ch.isbsib.proteomics.mzviz.controllers.matches.ProteinMatchController._
 import ch.isbsib.proteomics.mzviz.matches.SearchId
 import ch.isbsib.proteomics.mzviz.matches.models.ProteinMatchMultipleSearches
 import ch.isbsib.proteomics.mzviz.matches.services.ProteinMatchMongoDBService
-import com.wordnik.swagger.annotations.{ApiParam, ApiOperation, Api}
+import ch.isbsib.proteomics.mzviz.theoretical.AccessionCode
+import com.wordnik.swagger.annotations._
 import play.api.cache.Cached
 import play.api.libs.json.Json
 import play.api.mvc.Action
@@ -44,5 +46,28 @@ object ProteinMatchMultipleSearchesController extends MatchController {
 
     }
   }
+  @ApiOperation(nickname = "findAllProteinsForMultipleSearchIdsAndACs",
+    value = "find all proteins corresponding to given searchIds and Acs",
+    notes = """protein list""",
+    response = classOf[List[String]],
+    httpMethod = "GET")
+  def findAllProteinsForMultipleSearchIdsAndACs(
+                                           @ApiParam(value = """searchIds""") @PathParam("searchIds") searchIds: String,
+                                           @ApiParam(value = """ACs""") @PathParam("ACs") accessionCodes: String
+                                           ) = Cached(req => req.uri) {
+    Action.async {
+      val sids = queryParamSearchIds(searchIds)
+      val acs=accessionCodes.split(",").toList.map(AccessionCode.apply).toSet
+      for {
+        proteinMultipleList <- ProteinMatchMongoDBService().findAllProteinsBySearchIdsAndACs(sids,acs).map(
+          _.foldLeft( ProteinMatchMultipleSearches(Map()) )( (r, c) => r.add(c.searchId, c) )
+        )
+      } yield {
+        Ok(Json.toJson(proteinMultipleList))
+      }
+
+    }
+  }
+
 }
 
