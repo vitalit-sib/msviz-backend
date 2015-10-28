7f4fdc4970a6d91d41390f35bcee65621a176e4c
rmylonas <roman.mylonas@isb-sib.ch>
2015-10-28 12:59:18 +0100

    add tests for MySql

---
+++ test/ch/isbsib/proteomics/mzviz/experimental/services/ExpMs1MySqlDBServiceSpecs.scala
@@ -0,0 +1,166 @@
+package ch.isbsib.proteomics.mzviz.experimental.services
+
+import java.io.File
+
+import ch.isbsib.proteomics.mzviz.commons.{Intensity, RetentionTime, Moz, TempMongoDBForSpecs}
+import ch.isbsib.proteomics.mzviz.controllers.experimental.ExperimentalController._
+import ch.isbsib.proteomics.mzviz.experimental.RunId
+import ch.isbsib.proteomics.mzviz.experimental.importer.LoaderMzXML
+import ch.isbsib.proteomics.mzviz.experimental.models.{Ms1Entry, Ms1Peak}
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Millis, Seconds, Span}
+import org.specs2._
+import play.api.Play
+import play.api.libs.json.JsValue
+import play.api.test.FakeApplication
+import play.api.test.Helpers._
+import scala.slick.jdbc.meta._
+import play.api.db.slick.Config.driver.simple._
+
+
+/**
+ * @author Roman Mylonas, Trinidad Martin & Alexandre Masselot
+ *         copyright 2014-2015, SIB Swiss Institute of Bioinformatics
+ */
+
+
+object AppWithTestDb2 extends FakeApplication(additionalConfiguration =
+  Map(
+    ("db.default.driver") -> "org.h2.Driver",
+    ("db.default.url") -> (
+      //        "jdbc:h2:mem:play-test-" + scala.util.Random.nextInt +      // in memory database
+      "jdbc:h2:/tmp/play-test-" + scala.util.Random.nextInt +     // file based db that can be accessed using h2-browse in activator
+        ";MODE=PostgreSQL;DATABASE_TO_UPPER=false;DB_CLOSE_DELAY=-1")
+  ))
+
+
+
+class ExpMs1MySqlDBServiceSpecs extends mutable.Specification{
+
+  // setup table and database
+  step(Play.start(AppWithTestDb2))
+
+//    trait _Session extends specification.Scope with mutable.After {
+//      println("Opening session")
+//      val tableName = s"test${util.Random.nextInt}"
+//      implicit val session = Database.forURL(s"jdbc:h2:mem:$tableName", driver = "org.h2.Driver").createSession()
+//      println(s"...session: $session")
+//
+//      println("Creating schema")
+//      createSchema(session)
+//
+//      def after = {
+//        println("Closing session")
+//        session.close()
+//      }
+//    }
+//
+//    def createSchema(implicit session: Session) = {
+//      if (MTable.getTables("MS").list.isEmpty) {
+//        println("empty")
+//      }
+//    }
+
+
+    val ms1Dao = TableQuery[ExpMs1MySqlDBService]
+
+    def createSchema(implicit session: Session) = {
+      if(MTable.getTables("MS").list.isEmpty) {
+        Ms1Dao.ddl.create
+        println("table MS was created")
+      }else{
+        println("table MS already existed")
+      }
+    }
+
+  trait _Session extends specification.Scope with mutable.After {
+    println("Opening session")
+    val tableName = s"test${util.Random.nextInt}"
+    implicit val session = Database.forURL(s"jdbc:h2:mem:$tableName", driver = "org.h2.Driver").createSession()
+    println(s"...session: $session")
+
+    println("Creating schema")
+    createSchema(session)
+
+    def after = {
+      println("Closing session")
+      session.close()
+    }
+  }
+
+
+  def insertData(implicit session: Session, file: File, runId: RunId) = {
+    val entries = LoaderMzXML.parseFile(file, runId)
+
+    var nrInserted = 0
+
+    while (entries.hasNext) {
+      val current = entries.next()
+      val runID = current.spId.runId.value
+      val rt = current.retentionTime.value
+
+      current.peaks.foreach {
+        peak => val ms1 = Ms1Peak(runID, rt, peak.moz.value, peak.intensity.value)
+          ms1Dao.insert(ms1)
+          nrInserted += 1
+      }
+    }
+
+    nrInserted
+  }
+
+    "create tables" should {
+
+        "create schema" in new _Session{
+          ms1Dao.length.run mustEqual(0)
+        }
+    }
+
+  "insert Ms1" should {
+
+    "insert F001644_small" in new _Session{
+      val nrInserted = insertData(session, new File ("test/resources/ms1/F001644_small.mzXML"), RunId("hoho"))
+
+      nrInserted mustEqual(31771)
+      ms1Dao.length.run mustEqual(31771)
+    }
+
+    "insert and find" in new _Session{
+      val nrInserted = insertData(session, new File ("test/resources/ms1/F001644_small.mzXML"), RunId("hoho"))
+
+      val rtTol = 0.5
+      val moz = 519.14
+      val daltonTolerance = 0.3
+
+      val ms1List = ms1Dao.filter(ms => (ms.ref === "hoho")
+        && (ms.moz <= moz+daltonTolerance)
+        && ms.moz >= moz-daltonTolerance).list.map(m => Ms1Entry(RunId(m.ref), RetentionTime(m.rt), Intensity(m.int), Moz(m.moz))
+      )
+
+      val json = ExpMs1MongoDBService().extract2Lists(ms1List, rtTol)
+
+      val rts = (json \ "rt").as[List[JsValue]]
+      rts.length mustEqual(98)
+      rts(20).as[Double] mustEqual(5.97779)
+
+      val ints = (json \ "intensities").as[List[JsValue]]
+      ints(20).as[Double] mustEqual(5459280.5)
+    }
+
+  }
+
+  "insert and delete" in new _Session {
+    insertData(session, new File ("test/resources/ms1/F001644_small.mzXML"), RunId("hoho"))
+    insertData(session, new File ("test/resources/ms1/F001644_small.mzXML"), RunId("hihi"))
+
+    ms1Dao.length.run mustEqual(31771 * 2)
+
+    ms1Dao.filter(ms => (ms.ref === "hoho")).delete
+
+    ms1Dao.length.run mustEqual(31771)
+
+  }
+
+  step(Play.stop())
+
+}
