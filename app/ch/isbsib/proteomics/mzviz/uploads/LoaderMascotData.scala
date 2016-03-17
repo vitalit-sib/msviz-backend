68d92cdec579b1fea1fc22cd6d7c54a1392f5403
rmylonas <roman.mylonas@isb-sib.ch>
2016-03-17 11:12:38 +0100

    created empty class

---
+++ app/ch/isbsib/proteomics/mzviz/uploads/LoaderMascotData.scala
@@ -0,0 +1,11 @@
+package ch.isbsib.proteomics.mzviz.uploads
+
+/**
+ * @author Roman Mylonas & Trinidad Martin
+ *         copyright 2014-2015, SIB Swiss Institute of Bioinformatics
+ */
+class LoaderMascotData {
+
+  //def loadZip()
+
+}
