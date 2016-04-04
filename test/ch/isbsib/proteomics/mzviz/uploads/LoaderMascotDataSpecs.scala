fc2ca8f97e5ffcac29bf9d3c6b3b7529c57fb9a1
rmylonas <roman.mylonas@isb-sib.ch>
2016-04-04 11:15:45 +0200

    tests for mascot loader

--- test/ch/isbsib/proteomics/mzviz/uploads/LoaderMascotDataSpecs.scala
+++ test/ch/isbsib/proteomics/mzviz/uploads/LoaderMascotDataSpecs.scala
@@ -1,9 +1,9 @@
 package ch.isbsib.proteomics.mzviz.uploads
 
 
-import ch.isbsib.proteomics.mzviz.commons.TempMongoDBForSpecs
+import ch.isbsib.proteomics.mzviz.commons.{Moz, TempMongoDBForSpecs}
 import ch.isbsib.proteomics.mzviz.experimental.RunId
-import ch.isbsib.proteomics.mzviz.experimental.services.ExpMongoDBService
+import ch.isbsib.proteomics.mzviz.experimental.services.{ExpMs1BinMongoDBService, ExpMongoDBService}
 import ch.isbsib.proteomics.mzviz.matches.SearchId
 import ch.isbsib.proteomics.mzviz.matches.importer.LoaderMzIdent
 import ch.isbsib.proteomics.mzviz.matches.services.MatchMongoDBService
@@ -14,6 +14,7 @@ import java.io.{IOException, File}
 
 import reactivemongo.api.{MongoDriver, DefaultDB}
 
+import scala.concurrent.Future
 import scala.util.Random
 
 
@@ -26,29 +27,67 @@ class LoaderMascotDataSpecs extends Specification with ScalaFutures {
   implicit val defaultPatience =
     PatienceConfig(timeout = Span(15, Seconds), interval = Span(5000, Millis))
 
+  /**
+   * extends the temp mngodatabase and add a exp service above it
+   */
+  trait TempMongoDBService extends TempMongoDBForSpecs {
+    val loaderService = new LoaderMascotData(db)
 
-  "check required files" in {
+    val exp1Service = new ExpMs1BinMongoDBService(db)
+    val exp2Service = new ExpMongoDBService(db)
+    val matchService = new MatchMongoDBService(db)
+  }
 
-    val dir = new File ("test/resources/uploads/sample1")
+  "helper functions" should {
 
-    val requiredFilesMap = LoaderMascotData().getRequiredFiles(Set("mzid", "mgf", "mzml"), dir)
+    "check required files" in new TempMongoDBService {
 
-    requiredFilesMap.get("mzid").get.getName mustEqual ("sample1.mzid")
-    requiredFilesMap.get("mgf").get.getName mustEqual ("sample1.mgf")
-    requiredFilesMap.get("mzml").get.getName mustEqual ("sample1.mzML")
+      val dir = new File("test/resources/uploads/sample1")
+
+      val requiredFilesMap = loaderService.getRequiredFiles(Set("mzid", "mgf", "mzml"), dir)
+
+      requiredFilesMap.get("mzid").get.getName mustEqual ("sample1.mzid")
+      requiredFilesMap.get("mgf").get.getName mustEqual ("sample1.mgf")
+      requiredFilesMap.get("mzml").get.getName mustEqual ("sample1.mzML")
+
+    }
+
+    "check runId from path" in new TempMongoDBService {
+
+      val dir = new File("test/resources/uploads/sample1")
+
+      val runId: String = loaderService.getRunIdFromPath(dir).value
+
+      runId mustEqual ("sample1")
+
+    }
+
+  }
+
+  "load data" should {
+
+    "load unzipped" in new TempMongoDBService{
+
+      val unzipped = "test/resources/uploads"
+      val results: Future[Int] = loaderService.loadUnzipped(unzipped, 1)
+
+      results.futureValue mustEqual(2792)
+
+      // check ms1
+      val ms1List = exp1Service.findMs1EntryWithMozTol(RunId("sample1"), Moz(519.14), 0.3).futureValue
+      ms1List.size mustEqual(148)
+
+      val ms2List = exp2Service.findAllSpectraRefByrunId(Set(RunId("sample1"), RunId("sample2"))).futureValue
+      ms2List.size mustEqual(4)
+
+      val matchList = matchService.findAllSpectrumIdBySearchId(SearchId("sample1")).futureValue
+      matchList.size mustEqual(62)
+
+    }
 
   }
 
 
-  "check runId from path" in {
-
-    val dir = new File ("test/resources/uploads/sample1")
-
-    val runId = LoaderMascotData().getRunIdFromPath(dir)
-
-    runId mustEqual("sample1")
-
-  }
 
 
 }
